20c06264fd9f866fbb37abf668e98531b5dbe4a1
keitkalon <79155265+keitkalon@users.noreply.github.com>
2022-04-18 10:35:02 +0300

    test CommandService/ProfileController

---
+++ CommandService/Controllers/ProfilesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class ProfilesController : ControllerBase
+    {
+        public ProfilesController()
+        {
+
+        }
+        public ActionResult TestInBoundConnection()
+        {
+            Console.WriteLine("--> Inbound Post # Command Service");
+            return Ok("Inbound test from Profiles Controllers");
+        }
+    }
+}
