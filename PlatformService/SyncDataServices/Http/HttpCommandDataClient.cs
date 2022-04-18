1870bd68a1706a134a5c4ce469c98f69159ae940
keitkalon <79155265+keitkalon@users.noreply.github.com>
2022-04-18 12:13:40 +0300

    PlatformService/SyncDataService connection

---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,35 @@
+﻿using PlatformService.Dtos;
+using System.Text;
+using System.Text.Json;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+        private IConfiguration _configuration;
+
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+        async Task ICommandDataClient.SendProfileToCommand(ProfileReadDtos profile)
+        {
+            var httpContent = new StringContent(
+                JsonSerializer.Serialize(profile),
+                Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("http://localhost:5037/api/c/profiles", httpContent);
+            
+            
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("---> Sync POST to CommandService was OK!");
+            }
+            else
+            {
+                Console.WriteLine("---> Sync POST to CommandServices was NOT OK!");
+            }
+        }
+    }
+}
